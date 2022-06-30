99b629f87c1353f7640ff14fee9990771f34d4fe
pxilips <58148159+pxilips@users.noreply.github.com>
2022-06-30 15:50:04 +0300

    Create etc_dir.sh

---
+++ etc_dir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+IFS=:
+for folder in /etc
+do
+echo "$folder:"
+for file in $folder/*
+do
+if [ -d $file ]
+then
+echo " $file"
+fi
+done
+done > etc_dir.txt
+echo "Finished, look etc_dir.txt"
